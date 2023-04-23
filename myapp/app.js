316bf63142363789656d1173d0eecfdde4b43905
mnamadina <madina@brandeis.edu>
2023-04-23 19:58:58 -0400

    added router

--- myapp/app.js
+++ myapp/app.js
@@ -7,15 +7,13 @@ const layouts = require("express-ejs-layouts");
 const pw_auth_router = require("./routes/pwauth");
 const transactionsRouter = require("./routes/transactions");
 
-// init darkMode variables
-let darkMode = false;
+const User = require('./models/User');
 
 /* **************************************** */
 /*  Connecting to a Mongo Database Server   */
 /* **************************************** */
-const mongodb_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pwdemo";
-console.log("MONGODB_URI=", process.env.MONGODB_URI);
+const mongodb_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pwdemo';
+console.log('MONGODB_URI=',process.env.MONGODB_URI);
 
 const mongoose = require("mongoose");
 
@@ -31,16 +29,16 @@ db.once("open", () => {
 /* **************************************** */
 /* Enable sessions and storing session data in the database */
 /* **************************************** */
-const session = require("express-session");
-var MongoDBStore = require("connect-mongodb-session")(session);
+const session = require("express-session"); // to handle sessions using cookies 
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 const store = new MongoDBStore({
   uri: mongodb_URI,
-  collection: "mySessions",
+  collection: 'mySessions'
 });
 
-// catch errors
-store.on("error", (error) => {
+// Catch errors                                                                      
+store.on('error', function(error) {
   console.log(error);
 });
 
@@ -59,44 +57,52 @@ function isLoggedIn(req, res, next) {
 /* **************************************** */
 /* creating the app */
 /* **************************************** */
-const app = express();
+var app = express();
 
-app.use(
-  session({
-    secret: "This is a secret",
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-    },
-    store: store,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+app.use(session({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week                                        
+  },
+  store: store,
+  // Boilerplate options, see:                                                       
+  // * https://www.npmjs.com/package/express-session#resave                          
+  // * https://www.npmjs.com/package/express-session#saveuninitialized               
+  resave: true,
+  saveUninitialized: true
+}));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+app.use(pw_auth_router)
 
 app.use(layouts);
-app.use(pw_auth_router);
 
 app.get("/", (req, res, next) => {
-  res.render("index", { darkMode });
+  res.render("index");
 });
+
+
+app.get('/about', 
+  isLoggedIn,
+  (req,res,next) => {
+    res.render('about');
+  }
+)
 
 app.use(transactionsRouter);
 
-// dark mode toggle
-app.get("/toggle-dark-mode", (req, res) => {
-  darkMode = !darkMode;
-  res.redirect("/");
-});
 
 // catch 404 errors
 app.use((req, res, next) => {
@@ -104,12 +110,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
